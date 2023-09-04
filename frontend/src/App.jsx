2d61b450a48a585f9677c33c7b05d9f848037d2b
You Song Hou <ysongweb3@gmail.com>
2023-09-04 01:17:58 -0400

    refactor: remove App.css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import { ChakraProvider, Text } from '@chakra-ui/react';
 import { HashRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
 
 function App() {
 
