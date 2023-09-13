8cea646b7fd174662b2801eae9abcbc7042cfee5
You Song Hou <ysongweb3@gmail.com>
2023-09-12 22:27:13 -0400

    feat: add DaoDealClient from the FEVM Data DAO Kit

--- contract/scripts/deploy.js
+++ contract/scripts/deploy.js
@@ -12,9 +12,7 @@ async function main() {
 
   const lockedAmount = hre.ethers.parseEther("0.001");
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+  const lock = await hre.ethers.deployContract("DaoDealClient");
 
   await lock.waitForDeployment();
 
