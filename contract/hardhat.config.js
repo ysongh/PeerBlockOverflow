eba8106e98adf1deda3770db7a92c20b589fcc47
You Song Hou <ysongweb3@gmail.com>
2023-09-14 01:37:23 -0400

    feat: update 'hardhat.config.js' to deploy a contract on Calibration Testnet

--- contract/hardhat.config.js
+++ contract/hardhat.config.js
@@ -3,6 +3,15 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    // npx hardhat run scripts/deploy.js --network calibration
+    "calibration": {
+      url: "https://api.calibration.node.glif.io/rpc/v1",
+      accounts: [process.env.PRIVATEKEY],
+      chainId: 314159,
+      gasPrice: 8000000000
+    }
+  },
   paths: {
     artifacts: '../frontend/src/artifacts',
     cache: '../frontend/src/cache',
