3d384257bce6a2ab703686ee1d7ca990b4dd020b
You Song Hou <ysongweb3@gmail.com>
2023-09-16 01:14:58 -0400

    refactor: update UI for the posts page

--- frontend/src/pages/Posts.jsx
+++ frontend/src/pages/Posts.jsx
@@ -8,15 +8,15 @@ function Posts() {
   return (
     <Container maxW='1000px'>
       <Heading>Projects</Heading>
-      <SimpleGrid minChildWidth='120px' spacing='40px'>
-        <Box borderWidth='1px' borderRadius='lg' borderColor='green' overflow='hidden' p='5' width='500px' mt='5'>
+      <SimpleGrid minChildWidth='300px' spacing='5px'>
+        <Box borderWidth='1px' borderRadius='lg' borderColor='green' overflow='hidden' p='5' mt='5'>
           <Heading textAlign="center" fontSize="3xl" mb="4">Title</Heading>
           <Text textAlign="center" fontSize="xl" mb="4">Detail....</Text>
           <Tag>React</Tag>
           <br />
           <Button mt="4" onClick={() => navigate(`/post`)}>View</Button>
         </Box>
-        <Box borderWidth='1px' borderRadius='lg' borderColor='green' overflow='hidden' p='5' width='500px' mt='5'>
+        <Box borderWidth='1px' borderRadius='lg' borderColor='green' overflow='hidden' p='5' mt='5'>
           <Heading textAlign="center" fontSize="3xl" mb="4">Title</Heading>
           <Text textAlign="center" fontSize="xl" mb="4">Detail....</Text>
           <Tag>JS</Tag>
