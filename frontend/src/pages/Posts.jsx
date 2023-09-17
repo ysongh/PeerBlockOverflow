41344528e2fe994e9e05fbd22d38a8717a35d211
You Song Hou <ysongweb3@gmail.com>
2023-09-16 22:21:35 -0400

    feat: add a search box

--- frontend/src/pages/Posts.jsx
+++ frontend/src/pages/Posts.jsx
@@ -1,13 +1,21 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Container, SimpleGrid, Box, Heading, Text, Tag, Button } from '@chakra-ui/react';
+import { Container, SimpleGrid, Box, Heading, InputGroup, Input, InputRightElement, Text, Tag, Button } from '@chakra-ui/react';
 
 function Posts() {
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
+
+  const [search, setSearch] = useState("");
 
   return (
     <Container maxW='1000px'>
-      <Heading>Projects</Heading>
+      <Heading>Find Posts</Heading>
+      <InputGroup bg='white' mt='4'>
+        <Input placeholder='Search' value={search} onChange={(e) => setSearch(e.target.value)}/>
+        <InputRightElement>
+          <Text mr="2" color="blue" fontWeight="bold">Find</Text>
+        </InputRightElement>
+      </InputGroup>
       <SimpleGrid minChildWidth='300px' spacing='5px'>
         <Box borderWidth='1px' borderRadius='lg' borderColor='green' overflow='hidden' p='5' mt='5'>
           <Heading textAlign="center" fontSize="3xl" mb="4">Title</Heading>
