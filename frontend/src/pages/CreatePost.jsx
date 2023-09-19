676cd3f15e0baf5fd1204bf73c0efbb257d04fbd
You Song Hou <ysongweb3@gmail.com>
2023-09-18 21:52:51 -0400

    feat: install 'web3.storage' and use it to store post data on IPFS

--- frontend/src/pages/CreatePost.jsx
+++ frontend/src/pages/CreatePost.jsx
@@ -1,6 +1,11 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { FormControl, FormLabel, Box, ButtonGroup, Spinner, Input, Heading, Button } from '@chakra-ui/react';
+import { Web3Storage } from 'web3.storage';
+
+import { WEB3STORAGE_APIKEY } from '../../keys';
+
+const client = new Web3Storage({ token: WEB3STORAGE_APIKEY });
 
 function CreateCoupon() {
   const router = useNavigate();
@@ -14,6 +19,19 @@ function CreateCoupon() {
   const handleSubmit = async () => {
     try{
       console.log(title, description, tags);
+      const postData = JSON.stringify({ title, description, tags });
+      const blob = new Blob([postData], {type: "text/plain"});
+      const postDataFile = new File([ blob ], 'postData.json');
+
+      const cid = await client.put([postDataFile], {
+        onRootCidReady: localCid => {
+          console.log(`> 🔑 locally calculated Content ID: ${localCid} `)
+          console.log('> 📡 sending files to web3.storage ')
+        },
+        onStoredChunk: bytes => console.log(`> 🛰 sent ${bytes.toLocaleString()} bytes to web3.storage`)
+      })
+
+      console.log(`https://dweb.link/ipfs/${cid}`);
     } catch(error) {
      console.error(error);
      setLoading(false);
