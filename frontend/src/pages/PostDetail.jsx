bef41c00776dbcecd74227e9f4161cd1c1fcd2a7
You Song Hou <ysongweb3@gmail.com>
2023-10-23 22:45:37 -0400

    feat: add a form for creating a comment

--- frontend/src/pages/PostDetail.jsx
+++ frontend/src/pages/PostDetail.jsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { Container, Center, Box, Heading, Tag } from '@chakra-ui/react';
+import { Container, Center, Box, Heading, Tag, FormControl, FormLabel, Textarea, Button } from '@chakra-ui/react';
 
 function PostDetail({ contract }) {
   const { id } = useParams();
 
   const [post, setPost] = useState({});
+  const [description, setDescription] = useState('');
   
   useEffect(() => {
     const getPost = async () => {
@@ -20,6 +21,10 @@ function PostDetail({ contract }) {
     if (contract) getPost();
   }, [contract])
 
+  const handleSubmit = async () => {
+
+  }
+
   return (
     <Container maxW='1000px'>
       <Center>
@@ -28,6 +33,18 @@ function PostDetail({ contract }) {
           <p>{post.description}</p>
           <br />
           <Tag>{post.tags}</Tag>
+        </Box>
+      </Center>
+      <Center>
+        <Box borderWidth='1px' borderRadius='lg' borderColor='blue.400' overflow='hidden' p='5' width='500px' mt='5'>
+          <Heading fontSize='2xl' mb='3'>Add Comment</Heading>
+          <FormControl mb='3'>
+            <FormLabel htmlFor='description'>Description</FormLabel>
+            <Textarea id='description' rows={7} onChange={(e) => setDescription(e.target.value)}/>
+          </FormControl>
+          <Button colorScheme='blue' onClick={handleSubmit}>
+            Add
+          </Button>
         </Box>
       </Center>
     </Container>
