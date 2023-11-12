8ea17e029250f22c62553a6a78ef4445b90dd510
You Song Hou <ysongweb3@gmail.com>
2023-11-11 20:43:52 -0500

    feat: display the chain name

--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -1,11 +1,12 @@
 import { Link as ReactLink } from 'react-router-dom';
-import { Container, Box, Flex, Heading, Spacer, Button, Link } from '@chakra-ui/react';
+import { Container, Box, Flex, Heading, Spacer, Badge, Button, Link } from '@chakra-ui/react';
 import { ethers } from 'ethers';
 
 import PeerBlockOverflow from "../../artifacts/contracts/PeerBlockOverflow.sol/PeerBlockOverflow.json";
 
-const CALIBRATION_CONTRACT_ADDRESS = "0xFda2FCAB7c8c2FDB3Ef69C37Ee94f1e7A94f0eD3";
-const LOCALHOST_CONTRACT_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+// const CALIBRATION_CONTRACT_ADDRESS = "0xFda2FCAB7c8c2FDB3Ef69C37Ee94f1e7A94f0eD3";
+// const LOCALHOST_CONTRACT_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+const MUMBAI_CONTRACT_ADDRESS = "0x1db889583773d27aB583e5531E6FB8F26CE0dDC8";
 
 function Navbar({ ethAddress, setETHAddress, setContract }) {
 
@@ -14,7 +15,7 @@ function Navbar({ ethAddress, setETHAddress, setContract }) {
     setETHAddress(accounts[0]);
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const signer = provider.getSigner();
-    const c = new ethers.Contract(LOCALHOST_CONTRACT_ADDRESS, PeerBlockOverflow.abi, signer);
+    const c = new ethers.Contract(MUMBAI_CONTRACT_ADDRESS, PeerBlockOverflow.abi, signer);
     setContract(c);
   }
 
@@ -30,6 +31,7 @@ function Navbar({ ethAddress, setETHAddress, setContract }) {
           <Link as={ReactLink} to="/">Home</Link>
           <Link as={ReactLink} to="/create-post">Create Post</Link>
           <Spacer />
+          <p><Badge bgColor="#ff99fe" fontSize='.9rem'>Mumbai</Badge></p>
           <Button onClick={connectMetamask}>
             {ethAddress ? ethAddress.slice(0, 5) + "..." + ethAddress.slice(37, 42) : 'Connect Wallet'}
           </Button>
