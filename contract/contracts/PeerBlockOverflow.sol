4bc60f17d3b32ab54f7595aa3fb31eacec318689
You Song Hou <ysongweb3@gmail.com>
2023-11-22 01:54:18 -0500

    feat: implement receiver codes for Chainlink CCIP

--- contract/contracts/PeerBlockOverflow.sol
+++ contract/contracts/PeerBlockOverflow.sol
@@ -5,8 +5,9 @@ import {IRouterClient} from "@chainlink/contracts-ccip/src/v0.8/ccip/interfaces/
 import {OwnerIsCreator} from "@chainlink/contracts-ccip/src/v0.8/shared/access/OwnerIsCreator.sol";
 import {Client} from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.sol";
 import {LinkTokenInterface} from "@chainlink/contracts/src/v0.8/shared/interfaces/LinkTokenInterface.sol";
+import {CCIPReceiver} from "@chainlink/contracts-ccip/src/v0.8/ccip/applications/CCIPReceiver.sol";
 
-contract PeerBlockOverflow is OwnerIsCreator {
+contract PeerBlockOverflow is OwnerIsCreator, CCIPReceiver {
     error NotEnoughBalance(uint256 currentBalance, uint256 calculatedFees);
 
     IRouterClient private s_router;
@@ -14,6 +15,9 @@ contract PeerBlockOverflow is OwnerIsCreator {
 
     uint public postCount = 0;
     Post[] public posts;
+
+    bytes32 private s_lastReceivedMessageId;
+    string private s_lastReceivedText;
 
     struct Post{
         uint id;
@@ -39,9 +43,16 @@ contract PeerBlockOverflow is OwnerIsCreator {
         uint256 fees
     );
 
+    event MessageReceived(
+        bytes32 indexed messageId,
+        uint64 indexed sourceChainSelector,
+        address sender,
+        string text
+    );
+
     /// @param _router The address of the router contract.
     /// @param _link The address of the link contract.
-    constructor(address _router, address _link) {
+    constructor(address _router, address _link) CCIPReceiver(router) {
         s_router = IRouterClient(_router);
         s_linkToken = LinkTokenInterface(_link);
     }
@@ -115,5 +126,31 @@ contract PeerBlockOverflow is OwnerIsCreator {
 
         // Return the message ID
         return messageId;
+    }
+
+    /// handle a received message
+    function _ccipReceive(
+        Client.Any2EVMMessage memory any2EvmMessage
+    ) internal override {
+        s_lastReceivedMessageId = any2EvmMessage.messageId; // fetch the messageId
+        s_lastReceivedText = abi.decode(any2EvmMessage.data, (string)); // abi-decoding of the sent text
+
+        emit MessageReceived(
+            any2EvmMessage.messageId,
+            any2EvmMessage.sourceChainSelector, // fetch the source chain identifier (aka selector)
+            abi.decode(any2EvmMessage.sender, (address)), // abi-decoding of the sender address,
+            abi.decode(any2EvmMessage.data, (string))
+        );
+    }
+
+    /// @notice Fetches the details of the last received message.
+    /// @return messageId The ID of the last received message.
+    /// @return text The last received text.
+    function getLastReceivedMessageDetails()
+        external
+        view
+        returns (bytes32 messageId, string memory text)
+    {
+        return (s_lastReceivedMessageId, s_lastReceivedText);
     }
 }
